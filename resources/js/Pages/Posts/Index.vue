bc697ff3bdc5ba24b77eb2cf16b1c800c89ac448
Abdulganiyu Abubakar Sidiq <abubakarsidiq994@gmail.com>
2025-07-19 18:49:10 +0100

    post folder with index file and display all posts

---
+++ resources/js/Pages/Posts/Index.vue
@@ -0,0 +1,21 @@
+<template>
+
+    <AppLayout>
+        <Container> 
+            <ul class="divide-y">
+                 <li v-for="post in posts" :key="post.id" class="px-2 py-4">
+                    <span class="font-bold text-lg"> {{ post.title }} </span>
+                 </li>
+            </ul>
+        </Container>
+    </AppLayout>
+
+</template>
+
+
+<script setup>
+    defineProps(['posts']);
+    
+    import Container from '@/Components/Container.vue';
+    import AppLayout from '@/Layouts/AppLayout.vue';
+</script>
