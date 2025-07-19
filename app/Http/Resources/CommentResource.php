d0c11f816f1790f9e1d4bda987dc13bf74680064
Abdulganiyu Abubakar Sidiq <abubakarsidiq994@gmail.com>
2025-07-19 19:44:17 +0100

    add api resources for post, user and comment and also return the column that we want to display to user/guest, sensitize the column if user account is not logged in

---
+++ app/Http/Resources/CommentResource.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class CommentResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        // return parent::toArray($request);
+        return [
+            'id' => $this->id,
+            'body' => $this->body,
+            'user' => UserResource::make($this->user),
+            'post' => PostResource::make($this->post),
+            'updated_at' => $this->updated_at,
+            'created_at' => $this->created_at,
+        ];
+    }
+}
