cecd3deba5b5f36a27311bc44107ea29bd02a1c7
Abdulganiyu Abubakar Sidiq <abubakarsidiq994@gmail.com>
2025-09-17 19:22:17 +0100

    feat: implement CommentController with store method for adding comments to posts

---
+++ app/Http/Controllers/CommentController.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Comment;
+use App\Models\Post;
+use Illuminate\Http\Request;
+
+class CommentController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request, Post $post)
+    {
+        Comment::make($request->validate([
+            'body' => 'required|string|max:1000',
+        ]))
+            ->user()->associate($request->user())
+            ->post()->asssociate($post)
+            ->save();
+
+        return to_route('posts.show', $post);
+
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
